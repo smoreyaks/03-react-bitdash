97bfbd92a8a5c241aae2bbd22c96421d99d65ea8
smoreyaks <bnixon.eng@gmail.com>
2023-06-30 03:44:46 +1000

    Adding Components to Build

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { Routes, Route } from "react-router-dom";
+
 // Theme
 import { ColorModeContext, useMode } from "./theme";
 
@@ -5,8 +7,19 @@ import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 
 // Components
-// import Topbar from "./global/Topbar";
 import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+// import Team from "./scenes/team";
+// import Invoices from "./scenes/invoices";
+// import Contacts from "./scenes/contacts";
+// import Bar from "./scenes/bar";
+// import Form from "./scenes/form";
+// import Line from "./scenes/line";
+// import Pie from "./scenes/pie";
+// import FAQ from "./scenes/faq";
+// import Geography from "./scenes/geograpghy";
+// import Calendar from "./scenes/calendar";
 
 function App() {
     const [theme, colorMode] = useMode();
@@ -17,6 +30,19 @@ function App() {
                 <div className="app">
                     <main className="content">
                         <Topbar />
+                        <Routes>
+                            <Route path="/" element={<Dashboard />} />
+                            {/* <Route path="/team" element={<Team />} />
+                            <Route path="/contacts" element={<Contacts />} />
+                            <Route path="/invoices" element={<Invoices />} />
+                            <Route path="/form" element={<Form />} />
+                            <Route path="/bar" element={<Bar />} />
+                            <Route path="/pie" element={<Pie />} />
+                            <Route path="/line" element={<Line />} />
+                            <Route path="/faq" element={<FAQ />} />
+                            <Route path="/geography" element={<Geography />} />
+                            <Route path="/calendar" element={<Calendar />} /> */}
+                        </Routes>
                     </main>
                 </div>
             </ThemeProvider>
