bba61b9f6216631a41fa9449a73287afab8f1e50
smoreyaks <bnixon.eng@gmail.com>
2023-07-01 03:37:58 +1000

    Added Team Component

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
-// import Team from "./scenes/team";
+import Team from "./scenes/team";
 // import Invoices from "./scenes/invoices";
 // import Contacts from "./scenes/contacts";
 // import Bar from "./scenes/bar";
@@ -33,16 +33,16 @@ function App() {
                         <Topbar />
                         <Routes>
                             <Route path="/" element={<Dashboard />} />
-                            {/* <Route path="/team" element={<Team />} />
-                            <Route path="/contacts" element={<Contacts />} />
-                            <Route path="/invoices" element={<Invoices />} />
-                            <Route path="/form" element={<Form />} />
-                            <Route path="/bar" element={<Bar />} />
-                            <Route path="/pie" element={<Pie />} />
-                            <Route path="/line" element={<Line />} />
-                            <Route path="/faq" element={<FAQ />} />
-                            <Route path="/geography" element={<Geography />} />
-                            <Route path="/calendar" element={<Calendar />} /> */}
+                            <Route path="/team" element={<Team />} />
+                            {/* <Route path="/contacts" element={<Contacts />} /> */}
+                            {/* <Route path="/invoices" element={<Invoices />} /> */}
+                            {/* <Route path="/form" element={<Form />} /> */}
+                            {/* <Route path="/bar" element={<Bar />} /> */}
+                            {/* <Route path="/pie" element={<Pie />} /> */}
+                            {/* <Route path="/line" element={<Line />} /> */}
+                            {/* <Route path="/faq" element={<FAQ />} /> */}
+                            {/* <Route path="/geography" element={<Geography />} /> */}
+                            {/* <Route path="/calendar" element={<Calendar />} /> */}
                         </Routes>
                     </main>
                 </div>
