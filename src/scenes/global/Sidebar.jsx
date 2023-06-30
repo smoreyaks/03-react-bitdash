63806ab39529f37c26ab34d2cf2da25b5070f50e
smoreyaks <bnixon.eng@gmail.com>
2023-06-30 16:16:39 +1000

    Sidebar Collapsing Function & Button Display

---
+++ src/scenes/global/Sidebar.jsx
@@ -0,0 +1,97 @@
+// React Hooks
+import { useState } from "react";
+
+// React Router DOM
+import { Link } from "react-router-dom";
+
+// React Pro Sidebar
+import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
+import "react-pro-sidebar/dist/css/styles.css";
+
+// MUI Components
+import { Box, Typography, IconButton, useTheme } from "@mui/material";
+
+// Theme
+import { tokens } from "../../theme";
+
+// MUI Icons
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
+import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutlined";
+import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
+import { MenuOutlined } from "@mui/icons-material";
+
+const Sidebar = () => {
+    // useState
+    const [isCollapsed, setIsCollapsed] = useState("isCollapsed");
+    const theme = useTheme();
+    // Mode Color Access
+    const colors = tokens(theme.palette.mode);
+
+    return (
+        <Box
+            sx={{
+                "& .pro-sidebar-inner": {
+                    background: `${colors.primary[400]} !important`,
+                },
+                "& .pro-icon-wrapper": {
+                    backgroundColor: "transparent !important",
+                },
+                "& .pro-inner-item": {
+                    padding: "5px 35px 5px 20px !important",
+                },
+                "& .pro-inner-item:hover": {
+                    color: "#868dfb !important",
+                },
+                "& .pro-menu-item.active": {
+                    color: "#6870fa !important",
+                },
+            }}
+        >
+            <ProSidebar collapsed={isCollapsed}>
+                <Menu iconShape="square">
+                    {/* Menu & Logo Icons */}
+                    <MenuItem
+                        onClick={() => setIsCollapsed(!isCollapsed)}
+                        icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
+                        style={{
+                            margin: "10px 0 20px 0",
+                            color: colors.grey[100],
+                        }}
+                    >
+                        {!isCollapsed && (
+                            <Box
+                                display="flex"
+                                justifyContent="space-between"
+                                alignItems="center"
+                                ml="15px"
+                            >
+                                <Typography
+                                    variant="h3"
+                                    color={colors.grey[100]}
+                                >
+                                    ADMINIS
+                                </Typography>
+                                <IconButton
+                                    onClick={() => setIsCollapsed(!isCollapsed)}
+                                >
+                                    <MenuOutlinedIcon />
+                                </IconButton>
+                            </Box>
+                        )}
+                    </MenuItem>
+                </Menu>
+            </ProSidebar>
+        </Box>
+    );
+};
+
+export default Sidebar;
