7043c4869b86fb7a3df66b1e2dcbf43324771f4d
smoreyaks <bnixon.eng@gmail.com>
2023-07-05 19:26:14 +1000

    Fixed for Dashboard View

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -3,7 +3,7 @@ import { ResponsiveBar } from "@nivo/bar";
 import { tokens } from "../theme";
 import { mockBarData as data } from "../data/mockData";
 
-const BarChart = () => {
+const BarChart = ({ isDashboard = false }) => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
 
@@ -32,7 +32,7 @@ const BarChart = () => {
                         },
                     },
                 },
-                legend: {
+                legends: {
                     text: {
                         fill: colors.grey[100],
                     },
@@ -89,7 +89,7 @@ const BarChart = () => {
                 tickSize: 5,
                 tickPadding: 5,
                 tickRotation: 0,
-                legend: "country",
+                legend: isDashboard ? undefined : "country",
                 legendPosition: "middle",
                 legendOffset: 32,
             }}
@@ -97,7 +97,7 @@ const BarChart = () => {
                 tickSize: 5,
                 tickPadding: 5,
                 tickRotation: 0,
-                legend: "food",
+                legend: isDashboard ? undefined : "food",
                 legendPosition: "middle",
                 legendOffset: -40,
             }}
